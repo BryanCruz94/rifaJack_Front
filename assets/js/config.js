428e7871e92fe9ffe264aa18dfa867ffb36fba7a
Bryan Cruz <bscruz@espe.edu.ec>
2026-06-22 22:37:43 -0500

    Correcion url api

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,7 +3,7 @@ const ENVIRONMENT = "production";
 
 const API_URLS = {
     local: "http://localhost:3000",
-    production: "https://rifajackback-production.up.railway.app/"
+    production: "https://rifajackback-production.up.railway.app"
 };
 
 if (!API_URLS[ENVIRONMENT]) {
